6547d6834e64cf5352508c7b26719e4f5ef5c4fe
sunnavy <sunnavy@bestpractical.com>
2020-08-13 13:25:14 +0800

    Prep 0.21

--- lib/RT/Extension/TimeTracking.pm
+++ lib/RT/Extension/TimeTracking.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package RT::Extension::TimeTracking;
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 RT->AddStyleSheets("time_tracking.css");
 RT->AddJavaScript("time_tracking.js");
