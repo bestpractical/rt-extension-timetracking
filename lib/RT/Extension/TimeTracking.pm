db61fc85adbc03fd0b17c0994f9552c3556b681c
Jim Brandt <jbrandt@bestpractical.com>
2022-03-10 13:50:20 -0500

    Document RTIR support

--- lib/RT/Extension/TimeTracking.pm
+++ lib/RT/Extension/TimeTracking.pm
@@ -60,6 +60,7 @@ RT-Extension-TimeTracking - Time Tracking Extension
 =head1 RT VERSION
 
 Works with RT 5.0. Check 0.1* versions if you are using RT 4.
+Also supported in RTIR 5.0.3 and later.
 
 =head1 INSTALLATION
 
